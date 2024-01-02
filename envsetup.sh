fe8ce082d1d7d5d219a1f8eb9734b4269939fb81
pranjaldev <pranjaldev.goswami@novusinsights.com>
2024-01-02 14:54:07 +0530

    final prod code update

---
+++ envsetup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [-d "env"]
+then
+    echo "python3 virtual env exists"
+else
+    python3 -m venv env
+
+fi 
+echo $PWD
+
+source env/bin/activate
+
+pip3 install -r requirements.txt
+
+if [-d "logs"]
+then
+echo "log folder exists"
+
+else
+    mkdir logs
+    tocuh log/error.log logs/access.log
+
+fi
+
+sudo chmod -R 777 logs
+echo "envsetup finished"
